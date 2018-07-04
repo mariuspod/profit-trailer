eafa8b04d6772a6788dc4982196636cbe111f478
Marius Podwyszynski <marius.pod@gmail.com>
2018-07-05 01:20:07 +0200

    Binding to 127.0.0.1 and changed container_name.

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ PT_HOME=$1
 PT_NAME=$2
 PT_PORT=$3
 docker rm -f $PT_NAME
-docker run -d -p $PT_PORT:8081 --name $PT_NAME -v "$PT_HOME:/app/ProfitTrailer" profit_trailer
+docker run -d -p 127.0.0.1:$PT_PORT:8081 --name $PT_NAME -v "$PT_HOME:/app/ProfitTrailer" profit-trailer
 docker logs -f $PT_NAME
