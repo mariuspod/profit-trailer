f52e2405e17e66696262fb95de91b4abff8b8e3a
Marius Podwyszynski <marius.pod@gmail.com>
2018-07-05 01:21:29 +0200

    Added default docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,24 +4,24 @@ BASE=/opt
 APP=/app
 PT="ProfitTrailer"
 
-PT_DIR=$APP/$PT
-PT_ZIP=$BASE/${PT}.zip
-PT_JAR=$PT_DIR/${PT}.jar
+PT_DIR=${APP}/${PT}
+PT_ZIP=${BASE}/${PT}-${PT_VERSION}.zip
+PT_JAR=${PT_DIR}/${PT}.jar
 PT_START="java -jar $PT_JAR -XX:+UseConcMarkSweepGC -Xmx256m -Xms256m"
 
-[ -d "$PT_DIR" ] || mkdir "$PT_DIR" || {
+[ -d "$PT_DIR" ] || mkdir -p "$PT_DIR" || {
    echo "Error: no $PT_DIR found and could not make it. Exiting."; exit -1;
 }
-unzip -oqd $APP $PT_ZIP $PT/${PT}.jar || {
+unzip -joqd ${PT_DIR} ${PT_ZIP} ${PT}-${PT_VERSION}/${PT}.jar || {
   echo "Error: no $PT jar found. Exiting."; exit -1;
 }
-cd $PT_DIR || {
+cd ${PT_DIR} || {
   echo "Error: problem with $PT_DIR. Exiting."; exit -1;
 }
 
-pcnt=$(/bin/ls -1 $PT_DIR/*.properties 2>/dev/null|/usr/bin/wc -l)
-[[ $pcnt -gt 0 ]] || {
-  echo "No properties found, extracting..."; unzip -o $PT_ZIP -d $APP;
+pcnt=$(/bin/ls -1 ${PT_DIR}/*.properties 2>/dev/null|/usr/bin/wc -l)
+[[ ${pcnt} -gt 0 ]] || {
+  echo "No properties found, extracting..."; unzip -jo ${PT_ZIP} -d ${PT_DIR};
   echo "Done! Now, edit your configuration files and reload the container."
   exit -1;
 } || {
@@ -30,4 +30,4 @@ pcnt=$(/bin/ls -1 $PT_DIR/*.properties 2>/dev/null|/usr/bin/wc -l)
 }
 
 # start it
-$PT_START
+${PT_START}
